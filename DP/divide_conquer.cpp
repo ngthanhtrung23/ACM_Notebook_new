b61de91ce9497a6dff8b72b868572cdb36b173a6
thanhtnguyen <thanhtnguyen@paypal.com>
2014-10-08 17:13:00 +0800

    Add divide & conquer optimization

--- DP/divide_conquer.cpp
+++ DP/divide_conquer.cpp
@@ -13,3 +13,49 @@
 //       3. compute(i, L, M-1, optL, opt[i][M])
 //       4. compute(i, M+1, R, opt[i][M], optR)
 
+// Example: http://codeforces.com/contest/321/problem/E
+#include "../template.h"
+
+const int MN = 4011;
+const int inf = 1000111000;
+int n, k, cost[MN][MN], dp[811][MN];
+
+inline int getCost(int i, int j) {
+    return cost[j][j] - cost[j][i-1] - cost[i-1][j] + cost[i-1][i-1];
+}
+
+void compute(int i, int L, int R, int optL, int optR) {
+    if (L > R) return ;
+
+    int mid = (L + R) >> 1, savek = optL;
+    dp[i][mid] = inf;
+    FOR(k,optL,min(mid-1, optR)) {
+        int cur = dp[i-1][k] + getCost(k+1, mid);
+        if (cur < dp[i][mid]) {
+            dp[i][mid] = cur;
+            savek = k;
+        }
+    }
+    compute(i, L, mid-1, optL, savek);
+    compute(i, mid+1, R, savek, optR);
+}
+
+int main() {
+    ios :: sync_with_stdio(false); cin.tie(NULL);
+    while (cin >> n >> k) {
+        FOR(i,1,n) FOR(j,1,n) {
+            cin >> cost[i][j];
+            cost[i][j] = cost[i-1][j] + cost[i][j-1] - cost[i-1][j-1] + cost[i][j];
+        }
+
+        dp[0][0] = 0;
+        FOR(i,1,n) dp[0][i] = inf;
+
+        FOR(i,1,k) {
+            compute(i, 1, n, 0, n);
+        }
+        cout << dp[k][n] / 2 << endl;
+    }
+    return 0;
+}
+
