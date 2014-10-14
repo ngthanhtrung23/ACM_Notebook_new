2e218e710fc4f05ab361da7e08c4f6493ba6e859
Tmbao <tmbao@apcs.vn>
2014-10-14 11:37:14 +0700

    Add common area of two circle function.

--- Geometry/circle.h
+++ Geometry/circle.h
@@ -2,17 +2,18 @@ struct Circle : Point {
     double r;
     Circle(double x = 0, double y = 0, double r = 0) : Point(x, y), r(r) {}
 };
+
 // Find common tangents to 2 circles
 // Helper method
-void tangents(Point c, double r1, double r2, vector<Line> & ans) {
-    double r = r2 - r1;
+void tangents(Point c, double c1.r, double c2.r, vector<Line> & ans) {
+    double r = c2.r - c1.r;
     double z = sqr(c.x) + sqr(c.y);
     double d = z - sqr(r);
     if (d < -EPS)  return;
     d = sqrt(fabs(d));
     Line l((c.x * r + c.y * d) / z,
             (c.y * r - c.x * d) / z,
-            r1);
+            c1.r);
     ans.push_back(l);
 }
 // Actual method: returns vector containing all common tangents
@@ -63,8 +64,23 @@ vector<Point> intersection(Line l, Circle cir) {
     }
 }
 
-// Given 2 circles: (0, 0, r) and (x2, y2, r2)
+
+double commonCircleArea(Circle c1, Circle c2) { //return the common area of two circle
+    double d = hypot(c1.x-c2.x, c1.y-c2.y), area;
+    if (c1.r+c2.r <= d) area = 0;
+    else if (c2.r+d <= c1.r) area = (c1.r * c1.r - c2.r * c2.r) * PI;
+    else if (c1.r+d <= c2.r) area = (c2.r * c2.r - c1.r * c1.r) * PI;
+    else {
+        double p1 = c2.r * c2.r * acos((d*d + c2.r*c2.r - c1.r*c1.r) / (2*d*c2.r));
+        double p2 = c1.r * c1.r * acos((d*d + c1.r*c1.r - c2.r*c2.r) / (2*d*c1.r));
+        double p3 = 0.5 * sqrt((-d+c2.r+c1.r) * (d+c2.r-c1.r) * (d+c1.r-c2.r) * (d+c1.r+c2.r));
+        area = p1 + p2 - p3;
+    }
+    return area;
+}
+
+// Given 2 circles: (0, 0, r) and (c2.x, c2.y, c2.r)
 // Intersections are intersections with line Ax + By + C where
-// A = -2 * x2
-// B = -2 * y2
-// C = x2^2 + y2^2 + r^2 - r2^2
+// A = -2 * c2.x
+// B = -2 * c2.y
+// C = c2.x^2 + c2.y^2 + r^2 - c2.r^2
