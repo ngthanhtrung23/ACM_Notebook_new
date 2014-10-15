f7c77ffb7fbae63cc22211901196c54c3b04634b
thanhtnguyen <thanhtnguyen@paypal.com>
2014-10-15 17:53:36 +0800

    Add comment to rng_58 mincost flow

--- Graph/MaxFlow/MinCostMaxFlowDijkstra.h
+++ Graph/MaxFlow/MinCostMaxFlowDijkstra.h
@@ -1,3 +1,9 @@
+// Source: rng_58
+// Faster min cost max flow
+// Index from 0
+// NOTE!!!!!! Flow through both direction can be < 0
+// --> need to be careful when trace
+
 const long long F_INF = 1000111000111000LL;
 const long long C_INF = 1000111000111000LL;
 
