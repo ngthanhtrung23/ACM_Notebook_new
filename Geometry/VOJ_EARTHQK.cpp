0db814e2353074321b4e6fbcac0947a7d0ff2273
jarvis57 <jarvis57@users.noreply.github.com>
2014-10-15 17:25:56 +0800

    Create VOJ_EARTHQK.cpp

---
+++ Geometry/VOJ_EARTHQK.cpp
@@ -0,0 +1,36 @@
+// Accepted
+#include "../../template.h"
+#include "polygon.h"
+#include "basic.h"
+
+int test, x, y, m, n;
+
+int main() {
+	ios :: sync_with_stdio(false); cin.tie(NULL);
+
+	cin >> test;
+	while (test --) {
+		Polygon a;
+
+		cin >> n;
+		for (int i = 1; i <= n; i ++) {
+			cin >> x >> y;
+			a.push_back(Point(x, y));
+		}
+
+		cin >> x >> y;
+		Point p = Point(x, y);
+
+		cin >> m;
+		for (int i = 1; i <= m; i ++) {
+			int x1, y1, x2, y2;
+			cin >> x1 >> y1 >> x2 >> y2;
+			int k = ccw(Point(x1, y1), Point(x2, y2), p);
+			Line l = (k != -1) ? Line(Point(x1, y1), Point(x2, y2))
+							   : Line(Point(x2, y2), Point(x1, y1));
+			a = polygon_cut(a, l);
+		}
+
+		cout << (long long) (area(a) * 100) << endl;
+	}
+}
