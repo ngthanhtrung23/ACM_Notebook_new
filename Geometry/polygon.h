ac8add05de11dab8975113d57306c1a218d333a1
thanhtnguyen <thanhtnguyen@paypal.com>
2014-10-15 22:17:55 +0800

    Add nearest points

--- Geometry/polygon.h
+++ Geometry/polygon.h
@@ -186,9 +186,30 @@ double convex_diameter(Polygon pt) {
 }
 
 // Closest pair
-pair<Point,Point> closestPair(vector<Point> p) {
-    // Source: e-maxx.ru
-    return make_pair(Point(0, 0), Point(0, 0));
+// Source: e-maxx.ru
+void rec(int l, int r) {
+    if (r - l <= 3) {
+        for (int i=l; i<=r; ++i)
+            for (int j=i+1; j<=r; ++j)
+                    upd_ans (a[i], a[j]);
+        sort (a+l, a+r+1, &cmp_y);
+        return;
+    }
+
+    int m = (l + r) >> 1;
+    int midx = a[m].x;
+    rec (l, m),  rec (m+1, r);
+    static pt t[MAXN];
+    merge (a+l, a+m+1, a+m+1, a+r+1, t, &cmp_y);
+    copy (t, t+r-l+1, a+l);
+
+    int tsz = 0;
+    for (int i=l; i<=r; ++i)
+        if (abs (a[i].x - midx) < mindist) {
+            for (int j=tsz-1; j>=0 && a[i].y - t[j].y < mindist; --j)
+                upd_ans (a[i], t[j]);
+            t[tsz++] = a[i];
+        }
 }
 
 // Pick theorem
