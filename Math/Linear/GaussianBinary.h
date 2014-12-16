ecd4a8cc5595d58c10daac67466946be067fce0c
Trung, Nguyen Thanh <ngthanhtrung23@gmail.com>
2014-12-17 01:07:18 +0800

    Update Gauss indentation

--- Math/Linear/GaussianBinary.h
+++ Math/Linear/GaussianBinary.h
@@ -1,19 +1,19 @@
 int gauss (vector < bitset<N> > a, int n, int m, bitset<N> & ans) {
-	vector<int> where (m, -1);
-	for (int col=0, row=0; col<m && row<n; ++col) {
-		for (int i=row; i<n; ++i)
-			if (a[i][col]) {
-				swap (a[i], a[row]);
-				break;
-			}
-		if (! a[row][col])
-			continue;
-		where[col] = row;
+    vector<int> where (m, -1);
+    for (int col=0, row=0; col<m && row<n; ++col) {
+        for (int i=row; i<n; ++i)
+            if (a[i][col]) {
+                swap (a[i], a[row]);
+                break;
+            }
+        if (! a[row][col])
+            continue;
+        where[col] = row;
  
-		for (int i=0; i<n; ++i)
-			if (i != row && a[i][col])
-				a[i] ^= a[row];
-		++row;
-	}
-        // The rest of implementation is the same as above
+        for (int i=0; i<n; ++i)
+            if (i != row && a[i][col])
+                a[i] ^= a[row];
+        ++row;
+    }
+    // The rest of implementation is the same as above
 }
