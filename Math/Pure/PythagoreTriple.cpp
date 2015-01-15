1d006b7ac0149940cd8f9f21a79ab805b7715e6d
Trung, Nguyen Thanh <ngthanhtrung23@gmail.com>
2015-01-16 03:02:47 +0800

    Update Pythagorean triples

--- Math/Pure/PythagoreTriple.cpp
+++ Math/Pure/PythagoreTriple.cpp
@@ -1,21 +1,4 @@
 #include "template.h"
-// sinh bo 3 pytago voi x, y, z <= n
-vector< vector<int> > genPytTriples(int n) {
-    vector< vector<int> > ret;
-    for (int r=1; r*r<=n; ++r) for (int s=(r%2==0)?1:2; s<r; s+=2) if (__gcd(r,s)==1) {
-        vector<int> t,h;
-        t.push_back(r*r+s*s); //z 
-        t.push_back(2*r*s); // y
-        t.push_back(r*r-s*s); // x
-        h=t;
-        while (h[0]<=n) {
-            ret.push_back(h);        
-            h[0]+=t[0]; h[1]+=t[1]; h[2]+=t[2];            
-        }
-    }
-    sort(ret.begin(), ret.end());
-    return ret;
-}
 // sinh bo 3 pytago nguyen thuy voi x, y, z <= n
 vector< vector<int> > genPrimitivePytTriples(int n) {
     vector< vector<int> > ret;
@@ -29,3 +12,5 @@ vector< vector<int> > genPrimitivePytTriples(int n) {
     sort(ret.begin(), ret.end());
     return ret;
 }
+// a^2 + b^2 == c^2
+// a = m^2 - n^2, b = 2mn, c = m^2 + n^2
