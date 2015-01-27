d0be580a5d90ffa1dfab9694d9b874c4e98c4d90
Trung, Nguyen Thanh <thanhtnguyen@paypal.com>
2015-01-28 02:10:42 +0800

    Update Euler Path code

---
+++ Graph/Misc/EulerPath.h
@@ -0,0 +1,38 @@
+// If directed --> do not need reverse_edge, update add_edge method
+// No solution? NEED TO CHECK YOURSELF (connected graph, number of odd vertices)
+struct Edge;
+typedef list<Edge>::iterator iter;
+
+struct Edge {
+    int next_vertex;
+    iter reverse_edge;
+
+    Edge(int next_vertex)
+        :next_vertex(next_vertex) { }
+};
+
+const int max_vertices = 4011;
+int num_vertices;
+list<Edge> adj[max_vertices];       // adjacency list
+
+vector<int> path;
+
+void find_path(int v) {
+    while(adj[v].size() > 0) {
+        int vn = adj[v].front().next_vertex;
+        adj[vn].erase(adj[v].front().reverse_edge);
+        adj[v].pop_front();
+        find_path(vn);
+    }
+    path.push_back(v);
+}
+
+void add_edge(int a, int b) {
+    adj[a].push_front(Edge(b));
+    iter ita = adj[a].begin();
+    adj[b].push_front(Edge(a));
+    iter itb = adj[b].begin();
+    ita->reverse_edge = itb;
+    itb->reverse_edge = ita;
+}
+
