ca09b4d5d69dac84b4ed3ee62c4f2cbecdb4fcd8
Trung, Nguyen Thanh <thanhtnguyen@paypal.com>
2015-04-05 18:15:03 +0700

    Update zfunc implementation

--- String/zfunc.h
+++ String/zfunc.h
@@ -1,16 +1,14 @@
 // z[i] = độ dài xâu con dài nhất bắt đầu từ vị trí i mà trùng với đoạn đầu của v[]
-void zfunc(string v, vector<int> &z) {
-    int n = v.length(); z.resize(n);
-    int l = 0, r = -1;
-    z[0] = 0;
-    for(int i = 1; i < n-1; ++i) {
-        int k = (i > r) ? 0 : min(z[i - l], r - i + 1);
-        while (i + k < n && v[k] == v[i + k]) ++k;
-        z[i] = k;
-        if (i + k - 1 > r) {
-            l = i;
-            r = i + k - 1;
-        }
+vector<int> zfunc(string s) {
+    int n = (int) s.length();
+    vector<int> z(n);
+    for (int i = 1, l = 0, r = 0; i < n; ++i) {
+        if (i <= r)
+            z[i] = min (r - i + 1, z[i - l]);
+        while (i + z[i] < n && s[z[i]] == s[i + z[i]])
+            ++z[i];
+        if (i + z[i] - 1 > r)
+            l = i, r = i + z[i] - 1;
     }
-    z[0] = n;
+    return z;
 }
