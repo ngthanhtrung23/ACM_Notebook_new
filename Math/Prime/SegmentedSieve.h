4ce81d2991d6e6d8033742119f7c315f995873a7
Trung, Nguyen Thanh <thanhtnguyen@paypal.com>
2015-07-29 23:08:45 +0800

    Segmented Sieve should be able to run in long long

--- Math/Prime/SegmentedSieve.h
+++ Math/Prime/SegmentedSieve.h
@@ -1,19 +1,20 @@
 // table[i-L] == true <=> i == prime
-const int SQRTN = 1<<16; // upperbound of sqrt(H)
-vector<bool> segmentSieve(int L, int H) {
-    static int p[SQRTN], lookup = 0;
+const int SQRTN = 1<<16; // upperbound of sqrt(H) + 10
+vector<bool> segmentSieve(ll L, ll H) {
+    static ll p[SQRTN];
+    static int lookup = 0;
     if (!lookup) {
-        for (int i = 2; i < SQRTN; ++i) p[i] = i;
-        for (int i = 2; i*i < SQRTN; ++i)
+        for (ll i = 2; i < SQRTN; ++i) p[i] = i;
+        for (ll i = 2; i*i < SQRTN; ++i)
             if (p[i])
-                for (int j = i*i; j < SQRTN; j += i)
+                for (ll j = i*i; j < SQRTN; j += i)
                     p[j] = 0;
         remove(p, p+SQRTN, 0);
         lookup = 1;
     }
     vector<bool> table(H - L);
-    for (int i = L; i < H; ++i) table[i - L] = 1;
-    for (int i = 0, j; p[i] * p[i] < H; ++i) { // O( \sqrt(H) )
+    for (ll i = L; i < H; ++i) table[i - L] = 1;
+    for (ll i = 0, j; p[i] * p[i] < H; ++i) { // O( \sqrt(H) )
         if (p[i] >= L) j = p[i] * p[i];
         else if (L % p[i] == 0) j = L;
         else j = L - (L % p[i]) + p[i];
