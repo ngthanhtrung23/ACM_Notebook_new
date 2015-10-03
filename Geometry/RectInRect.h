9e9c2d333a11fd3bdcb1fa2fc88179499cf6ba6b
Trung, Nguyen Thanh <thanhtnguyen@paypal.com>
2015-10-03 11:58:29 +0800

    Fix indentation

--- Geometry/RectInRect.h
+++ Geometry/RectInRect.h
@@ -2,19 +2,19 @@
 // Not tested with doubles but should work fine :)
 // Code as written rejects rectangles that just touch.
 bool rect_in_rect(int X, int Y, int x, int y) {
-   if (Y > X) swap(Y, X);
-   if (y > x) swap(y, x);
-   double diagonal = sqrt(double(X)*X + double(Y)*Y);
-   if (x < X && y < Y) return true;
-   else if (y >= Y || x >= diagonal) return false;
-   else {
-      double w, theta, tMin = PI/4, tMax = PI/2;
-      while (tMax - tMin > EPS) {
-         theta = (tMax + tMin)/2.0;
-         w = (Y-x*cos(theta))/sin(theta);
-         if (w < 0 || x * sin(theta) + w * cos(theta) < X) tMin = theta;
-         else tMax = theta;
-      }
-      return (w > y);
-   }
+    if (Y > X) swap(Y, X);
+    if (y > x) swap(y, x);
+    double diagonal = sqrt(double(X)*X + double(Y)*Y);
+    if (x < X && y < Y) return true;
+    else if (y >= Y || x >= diagonal) return false;
+    else {
+        double w, theta, tMin = PI/4, tMax = PI/2;
+        while (tMax - tMin > EPS) {
+            theta = (tMax + tMin)/2.0;
+            w = (Y-x*cos(theta))/sin(theta);
+            if (w < 0 || x * sin(theta) + w * cos(theta) < X) tMin = theta;
+            else tMax = theta;
+        }
+        return (w > y);
+    }
 }
