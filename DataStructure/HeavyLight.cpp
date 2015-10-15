89c91cc37c7aeb89870f4804ed4c6a1554098883
Trung, Nguyen Thanh <thanhtnguyen@paypal.com>
2015-10-16 02:52:38 +0800

    Update HLD

--- DataStructure/HeavyLight.cpp
+++ DataStructure/HeavyLight.cpp
@@ -1,138 +1,57 @@
+// Tested:
+// - http://codeforces.com/gym/100739/problem/G
+// Notes:
+// - Index from 1
+// - dfn: vertex id --> position
+// --> for point query, only need to visit(dfn[u])
+// --> for range query, use query(u, v)
+// Author: zimpha
 #include <bits/stdc++.h>
-#define FOR(i,a,b) for(int i=(a); i<=(b); ++i)
-#define FORD(i,a,b) for(int i=(a); i>=(b); --i)
 using namespace std;
-
 const int MN = 100111;
-int n;                             // number of vertices
-vector<int> a[MN];                 // adjacency list
+vector<int> G[MN];
+int sz[MN], dep[MN], fa[MN];
+int dfn[MN], top[MN];
+int n, m, id;
 
-struct SegmentTree {               // 0-based indices
-    SegmentTree(int from, int to) {
+// Operations on segment tree
+int querySeg(int i, int l, int r, int u, int v) {return 0;}
+
+void dfs1(int u, int f = 0) {
+    sz[u] = 1; fa[u] = f;
+    for (auto &v: G[u]) if (v != f) {
+        dep[v] = dep[u] + 1;
+        dfs1(v, u); sz[u] += sz[v];
     }
+}
 
-    void update(int from, int to) {
+void dfs2(int u, int chain, int f = 0) {
+    int son(-1); dfn[u] = ++id; top[u] = chain;
+    for (auto &v: G[u]) if (v != f) {
+        if (son == -1 || sz[son] < sz[v]) son = v;
     }
-};
-
-const int MAX_LOG = 18;
-
-int father[MAX_LOG + 1][MN];
-struct LCA { // Index from 1
-    int h[MN];
-
-public:
-    LCA(int root) {
-        h[root] = 1;
-        father[0][root] = -1;
-        dfs(root, -1);
-
-        FOR(i,1,MAX_LOG) {
-            FOR(u,1,n) { // Change this if index from 0
-                if (father[i-1][u] < 0) father[i][u] = -1;
-                else father[i][u] = father[i-1][ father[i-1][u] ];
-            }
-        }
+    if (~son) dfs2(son, chain, u);
+    for (auto &v: G[u]) if (v != f && v != son) {
+        dfs2(v, v, u);
     }
+}
 
-    int get(int u, int v) {
-        if (u == v) return u;
-        if (h[u] > h[v]) swap(u, v);
-
-        if (h[u] != h[v]) {
-            FORD(i,MAX_LOG,0) {
-                int x = father[i][v];
-                if (x < 0) continue;
-                if (h[x] >= h[u]) {
-                    v = x;
-                }
-            }
-        }
-        if (u == v) return u;
-
-        FORD(i,MAX_LOG,0) {
-            if (father[i][u] != father[i][v]) {
-                u = father[i][u];
-                v = father[i][v];
-            }
-        }
-        return father[0][u];
+int query(int u, int v) {
+    int res = 0;
+    int fu = top[u], fv = top[v];
+    while (fu != fv) {
+        if (dep[fu] < dep[fv]) swap(u, v), swap(fu, fv);
+        res += querySeg(1, 1, n, dfn[fu], dfn[u]);
+        u = fa[fu], fu = top[u];
     }
+    if (dep[u] > dep[v]) swap(u, v);
+    res += querySeg(1, 1, n, dfn[u], dfn[v]);
+    return res;
+}
 
-private:
-    void dfs(int u, int fu) {
-        for(int v : a[u]) if (v != fu) {
-            h[v] = h[u] + 1;
-            father[0][v] = u;
-            dfs(v, u);
-        }
-    }
-};
+int main() {
+    dfs1(1);
+    id = 0;
+    dfs2(1, 1);
+}
 
-int numNode[MN];               // size of T(u)
-int par[MN];                   // u --> parent(u)
-int depth[MN];                 // u --> depth(u)
-int heavyUp[MN];               // = 0 if this is 1st in chain
-int heavyDown[MN];             // next vertex in chain
-int firstOnChain[MN];          // chain --> 1st vertex
-int idChain[MN];               // vertex --> chain
-int posChain[MN];              // vertex --> position in chain
-int sizeChain[MN];             // chain --> its size
-
-int numChain;
-vector<SegmentTree> trees;
-
-// Note:
-// - The following implementation is for IT on nodes
-// - If need to update / query on edges:
-// - - Add 1 array to store values of edges going from firstOnChain[idChain[v]] --> its parent
-// - - All trees.update call need to be (from, to] instead of [from, to]
-struct HeavyLight { // 1-based index
-public:
-    HeavyLight(int root) {
-        visit(root);
-        initHeavyLight();
-    }
-    void udpateTree(int v, int u, int lca) {
-        updateTreeAncestor(v, lca);
-        updateTreeAncestor(u, lca);
-    }
-    void updateTreeAncestor(int v, int u) { // u is ancestor of v
-        while (1) {
-            if (idChain[u] == idChain[v]) {
-                trees[idChain[u]].update(posChain[u], posChain[v]);
-                return;
-            }
-            trees[idChain[v]].update(0, posChain[v]);
-            v = par[firstOnChain[idChain[v]]];
-        }
-    }
-
-private:
-    void visit(int x) {
-        numNode[x] = 1;
-        for (auto y : a[x]) {
-            if (y == par[x]) continue;
-            par[y] = x;
-            depth[y] = depth[x] + 1;
-            visit(y);
-            numNode[x] += numNode[y];
-        }
-        for (auto y : a[x])
-            if (y != par[x] && numNode[y] * 2 >= numNode[x])
-                heavyUp[y] = 1, heavyDown[x] = y;
-    }
-    void initHeavyLight() {
-        for (int i = 1; i <= n; i++) {
-            int x = i;
-            if (heavyUp[x]) continue;
-            firstOnChain[numChain] = x;
-            while (x) {
-                idChain[x] = numChain;
-                posChain[x] = sizeChain[numChain]++;
-                x = heavyDown[x];
-            }
-            trees.push_back(SegmentTree(0, sizeChain[numChain++] - 1));
-        }
-    }
-};
