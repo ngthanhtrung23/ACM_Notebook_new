afed27ad94414e1a6c8f0ec7f4972aa87a535c4b
Trung Nguyen <ngthanhtrung23@gmail.com>
2018-09-29 17:11:33 +0800

    Fix parantheses in macro.

--- Math/Prime/IterativeSieve.h
+++ Math/Prime/IterativeSieve.h
@@ -3,8 +3,8 @@ typedef unsigned int uint;
 // NOTE: gP(n) is incorrect for even values of n
 #define N 90000000
 uint mark[N / 64 + 1];
-#define gP(n) (mark[n>>6]&(1<<((n>>1)&31)))
-#define rP(n) (mark[n>>6]&=~(1<<((n>>1)&31)))
+#define gP(n) (mark[(n)>>6]&(1<<(((n)>>1)&31)))
+#define rP(n) (mark[(n)>>6]&=~(1<<(((n)>>1)&31)))
 
 // prime indexed from 0
 uint prime[5222222], nprime;
