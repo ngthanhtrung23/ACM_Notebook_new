200cc67e814a1a6142e8b988a61edcfb3853e446
Trung Nguyen <ngthanhtrung23@gmail.com>
2018-10-04 14:23:39 +0800

    Improve mulMod and powMod.

--- Math/Prime/RabinMiller.h
+++ Math/Prime/RabinMiller.h
@@ -1,17 +1,40 @@
 typedef long long ll;
 
-ll mulMod(ll x, ll y, ll p) {
-    if (y == 0) return 0;
-    if (x < 1000111000111000111LL / y) return x * y % p;
-    ll mid = mulMod((x+x)%p, y>>1LL, p);
-    if (y & 1) return (mid + x) % p;
-    else return mid;
+/**
+ * When MOD < 2^63, use following mulMod:
+ * Source: https://en.wikipedia.org/wiki/Modular_arithmetic#Example_implementations
+ * On computer architectures where an extended precision format with at least 64 bits
+ * of mantissa is available (such as the long double type of most x86 C compilers),
+ * the following routine is faster than any algorithmic solution, by employing the
+ * trick that, by hardware, floating-point multiplication results in the most
+ * significant bits of the product kept, while integer multiplication results in the
+ * least significant bits kept
+ */
+uint64_t mulMod(uint64_t a, uint64_t b, uint64_t m) {
+    long double x;
+    uint64_t c;
+    int64_t r;
+
+    if (a >= m) a %= m;
+    if (b >= m) b %= m;
+
+    x = a;
+    c = x * b / m;
+    r = (int64_t)(a * b - c * m) % (int64_t)m;
+    return r < 0 ? r + m : r;
 }
-ll powMod(ll x, ll k, ll m) {
-    if (k == 0) return 1;
-    if ((k & 1)) return mulMod(x,powMod(x, k-1, m), m);
-    else return powMod(mulMod(x,x,m), k/2, m);
+
+/** Calculates a^b % m */
+uint64_t powMod(uint64_t a, uint64_t b, uint64_t m) {
+    uint64_t r = m==1?0:1; // make it works when m == 1.
+    while (b > 0) {
+        if (b & 1) r = mulMod(r, a, m);
+        b = b >> 1;
+        a = mulMod(a, a, m);
+    }
+    return r;
 }
+
 bool suspect(ll a, ll s, ll d, ll n) {
     ll x = powMod(a, d, n);
     if (x == 1) return true;
@@ -23,7 +46,7 @@ bool suspect(ll a, ll s, ll d, ll n) {
 }
 // {2,7,61,-1}                      is for n < 4759123141 (= 2^32)
 // {2,3,5,7,11,13,17,19,23,-1} is for n < 10^15 (at least)
-bool isPrime(ll n) {
+bool isPrime(int64_t n) {
     if (n <= 1 || (n > 2 && n % 2 == 0)) return false;
     ll test[] = {2,3,5,7,11,13,17,19,23,-1};
     ll d = n - 1, s = 0;
