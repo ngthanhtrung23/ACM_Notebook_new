bf188945c5da51be14179bd22150a028ff80481a
Trung Nguyen <ngthanhtrung23@gmail.com>
2018-10-15 11:00:44 +0800

    Make mulMod and powMod easier to find.

--- Math/modulo.h
+++ Math/modulo.h
@@ -1,8 +1,3 @@
-typedef long long ll;
-
-// mulMod and powMod is same as Math/modulo.h.
-// These 2 functions are duplicated here for easier copy-paste.
-
 /**
  * When MOD < 2^63, use following mulMod:
  * Source: https://en.wikipedia.org/wiki/Modular_arithmetic#Example_implementations
@@ -38,24 +33,3 @@ uint64_t powMod(uint64_t a, uint64_t b, uint64_t m) {
     return r;
 }
 
-bool suspect(ll a, ll s, ll d, ll n) {
-    ll x = powMod(a, d, n);
-    if (x == 1) return true;
-    for (int r = 0; r < s; ++r) {
-        if (x == n - 1) return true;
-        x = mulMod(x, x, n);
-    }
-    return false;
-}
-// {2,7,61,-1}                      is for n < 4759123141 (= 2^32)
-// {2,3,5,7,11,13,17,19,23,-1} is for n < 10^15 (at least)
-bool isPrime(int64_t n) {
-    if (n <= 1 || (n > 2 && n % 2 == 0)) return false;
-    ll test[] = {2,3,5,7,11,13,17,19,23,-1};
-    ll d = n - 1, s = 0;
-    while (d % 2 == 0) ++s, d /= 2;
-    for (int i = 0; test[i] < n && test[i] != -1; ++i)
-        if (!suspect(test[i], s, d, n)) return false;
-    return true;
-}
-// Killer prime: 5555555557LL (fail when not used mulMod)
