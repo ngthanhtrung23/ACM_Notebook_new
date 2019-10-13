cab0c2f19626a1b9fc07a9797231bf75dc2043ab
Trung Nguyen <ngthanhtrung23@gmail.com>
2019-10-13 14:36:50 +0800

    Bugfix: cubic equation when |z| <= EPS.

--- Math/Pure/Cubic.h
+++ Math/Pure/Cubic.h
@@ -1,3 +1,4 @@
+const double EPS = 1e-6;
 struct Result {
     int n; // Number of solutions
     double x[3]; // Solutions
@@ -8,7 +9,7 @@ Result solve_cubic(double a, double b, double c, double d) {
     long double r = (2*a1*a1*a1 - 9*a1*a2 + 27*a3)/54.0;
     double z = r*r-q*q*q, theta;
     Result s;
-    if(z <= 0) {
+    if(z <= EPS) {
         s.n = 3; theta = acos(r/sqrt(q*q*q));
         s.x[0] = sq*cos(theta/3.0) - a1/3.0;
         s.x[1] = sq*cos((theta+2.0*PI)/3.0) - a1/3.0;
