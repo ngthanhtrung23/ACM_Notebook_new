b3e56f9ddaf92ce9ba1c019aef20933bb3fd7272
Le Duy Thuc <leduykhongngu@gmail.com>
2020-01-15 18:45:23 +0700

    DSU_rollback: update join method
    Update join method to union by rank.

--- DataStructure/DSU_rollback.h
+++ DataStructure/DSU_rollback.h
@@ -1,4 +1,8 @@
-// TODO: Test with http://uoj.ac/problem/3
+#include <bits/stdc++.h>
+
+using namespace std;
+// Tested with https://codeforces.com/gym/100551/problem/A
+// 0-based
 struct Data {
     int time, u, par;  // before `time`, `par` = par[u]
 };
@@ -8,36 +12,37 @@ struct DSU {
     vector<Data> change;
 
     void init(int n) {
-        par = vector<int> (n + 5, 0);
-        REP(i, n+5) par[i] = i;
+        par = vector<int> (n + 5, -1);
         change.clear();
     }
 
     // find root of x.
-    // t should be current time. We use it to update `change`.
-    int getRoot(int x, int t) {
-        if (par[x] != x) {
-            change.push_back({t, x, par[x]});
-            par[x] = getRoot(par[x], t);
-        }
-        return par[x];
+    // if par[x] < 0 then x is a root, and its tree has -par[x] nodes
+    int getRoot(int x) {
+        while (par[x] >= 0)
+            x = par[x];
+        return x;
     }
 
     // join components containing x and y.
     // t should be current time. We use it to update `change`.
     void join(int x, int y, int t) {
-        x = getRoot(x, t);
-        y = getRoot(y, t);
-        // TODO: Possible TLE here? should use union by rank?
-        if (x != y) {
-            change.push_back({t, x, par[x]});
-            par[x] = y;
-        }
+        x = getRoot(x);
+        y = getRoot(y);
+        //union by rank
+        if (par[x] < par[y]) swap(x, y); 
+        //now x's tree has less nodes than y's tree
+        change.push_back({t, y, par[y]});
+        par[y] += par[x];
+        change.push_back({t, x, par[x]});
+        par[x] = y;
+        
+
     }
 
-    // rollback all changes at time >= t.
+    // rollback all changes at time > t.
     void rollback(int t) {
-        while (!change.empty() && change.back().time == t) {
+        while (!change.empty() && change.back().time > t) {
             par[change.back().u] = change.back().par;
             change.pop_back();
         }
