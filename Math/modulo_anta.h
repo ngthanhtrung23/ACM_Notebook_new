76237802f2b48096ddda6021fdf06bbfe23f5adc
Trung Nguyen <ngthanhtrung23@gmail.com>
2020-02-07 21:50:17 +0800

    Minor update in modulo.

--- Math/modulo_anta.h
+++ Math/modulo_anta.h
@@ -1,50 +1,73 @@
-// Author: anta
+// Modified from anta's code
 // Not tested with MOD > 10^9 + 7.
+// Tested:
+// - https://codeforces.com/gym/101383 - F (MOD = 1e9+7, +, *)
 
 inline void fasterLLDivMod(unsigned long long x, unsigned y, unsigned &out_d, unsigned &out_m) {
-	unsigned xh = (unsigned)(x >> 32), xl = (unsigned)x, d, m;
+    unsigned xh = (unsigned)(x >> 32), xl = (unsigned)x, d, m;
 #ifdef __GNUC__
-	asm(
-		"divl %4; \n\t"
-		: "=a" (d), "=d" (m)
-		: "d" (xh), "a" (xl), "r" (y)
-	);
+    asm(
+        "divl %4; \n\t"
+        : "=a" (d), "=d" (m)
+        : "d" (xh), "a" (xl), "r" (y)
+    );
 #else
-	__asm {
-		mov edx, dword ptr[xh];
-		mov eax, dword ptr[xl];
-		div dword ptr[y];
-		mov dword ptr[d], eax;
-		mov dword ptr[m], edx;
-	};
+    __asm {
+        mov edx, dword ptr[xh];
+        mov eax, dword ptr[xl];
+        div dword ptr[y];
+        mov dword ptr[d], eax;
+        mov dword ptr[m], edx;
+    };
 #endif
-	out_d = d; out_m = m;
+    out_d = d; out_m = m;
 }
 
 template<int MOD>
 struct ModInt {
-	static const int Mod = MOD;
-	unsigned x;
-	ModInt() : x(0) { }
-	ModInt(signed sig) { int sigt = sig % MOD; if (sigt < 0) sigt += MOD; x = sigt; }
-	ModInt(signed long long sig) { int sigt = sig % MOD; if (sigt < 0) sigt += MOD; x = sigt; }
-	int get() const { return (int)x; }
+    unsigned x;
 
-	ModInt &operator+=(ModInt that) { if ((x += that.x) >= MOD) x -= MOD; return *this; }
-	ModInt &operator-=(ModInt that) { if ((x += MOD - that.x) >= MOD) x -= MOD; return *this; }
-	ModInt &operator*=(ModInt that) {
-		unsigned dummy;
-		fasterLLDivMod((unsigned long long)x * that.x, MOD, dummy, x);
-		return *this;
-	}
-	ModInt operator*(ModInt that) const {
-		ModInt res;
-		unsigned dummy;
-		fasterLLDivMod((unsigned long long)x * that.x, MOD, dummy, res.x);
-		return res;
-	}
-	ModInt operator+(ModInt that) const { return ModInt(*this) += that; }
-	ModInt operator-(ModInt that) const { return ModInt(*this) -= that; }
+    constexpr ModInt() : x(0) { }
+    constexpr ModInt(signed sig) { int sigt = sig % MOD; if (sigt < 0) sigt += MOD; x = sigt; }
+    constexpr ModInt(signed long long sig) { int sigt = sig % MOD; if (sigt < 0) sigt += MOD; x = sigt; }
+
+#define COMPAREOP(OP) bool constexpr operator OP(ModInt b) const { return x OP b.x; }
+    COMPAREOP(==) COMPAREOP(!=) COMPAREOP(<) COMPAREOP(>) COMPAREOP(<=) COMPAREOP(>=)
+#undef COMPAREOP
+
+    ModInt operator-() const { return ModInt(x ? MOD - x : 0); }
+
+    ModInt constexpr& operator+=(ModInt that) { if ((x += that.x) >= MOD) x -= MOD; return *this; }
+    ModInt constexpr& operator-=(ModInt that) { if ((x += MOD - that.x) >= MOD) x -= MOD; return *this; }
+    ModInt &operator*=(ModInt that) {
+        unsigned dummy;
+        fasterLLDivMod((unsigned long long)x * that.x, MOD, dummy, x);
+        return *this;
+    }
+    ModInt operator*(ModInt that) const {
+        ModInt res;
+        unsigned dummy;
+        fasterLLDivMod((unsigned long long)x * that.x, MOD, dummy, res.x);
+        return res;
+    }
+    ModInt operator+(ModInt that) const { return ModInt(*this) += that; }
+    ModInt operator-(ModInt that) const { return ModInt(*this) -= that; }
+
+    // Below: copied from user202729_, not tested.
+    ModInt inv() const {
+        int a = x, b = MOD, ax = 1, bx = 0;
+        while (b) {
+            int q = a/b, t = a%b;
+            a = b; b = t;
+            t = ax - bx*q;
+            ax = bx; bx = t;
+        }
+        assert(a == 1);
+        if (ax < 0) ax += MOD;
+        return ax;
+    }
+    ModInt& operator /= (ModInt m) { return (*this) *= m.inv(); }
+    ModInt operator / (ModInt that) const { return ModInt(*this) /= that; }
 };
 const int MOD = 1e9 + 7;
 using modular = ModInt<MOD>;
