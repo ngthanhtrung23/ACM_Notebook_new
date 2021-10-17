f9dc9f23ab2fa7a04f39667d8c0120472262dfdb
Trung Nguyen <ngthanhtrung23@gmail.com>
2021-10-18 02:48:58 +0800

    Update matrix
    - template typename
    - gauss
    - inverse

--- Math/Matrix.h
+++ Math/Matrix.h
@@ -1,32 +1,125 @@
-struct Matrix {
-    vector< vector<long long> > x;
-    int m, n;
+// Tested: https://oj.vnoi.info/problem/dtl_lsr
 
-    Matrix(int m = 0, int n = 0) : m(m), n(n) {
+template<typename T>
+struct Matrix {
+    vector< vector<T> > x;
+
+    Matrix() {
+        x.clear();
+    }
+    Matrix(int m, int n) {
         x.resize(m);
         REP(i,m) {
             x[i].resize(n);
             REP(j,n) x[i][j] = 0;
         }
     }
+
+    const vector<T>& operator [] (size_t i) const {
+        return x[i];
+    }
+    
+    vector<T>& operator [] (size_t i) {
+        return x[i];
+    }
+
     void print() {
-        REP(i,m) {
-            REP(j,n) cout << x[i][j] << ' ';
+        REP(i,SZ(x)) {
+            REP(j,SZ(x[i])) cout << x[i][j] << ' ';
             cout << endl;
         }
     }
+
+    Matrix transpose() const {
+        int nRow = SZ(x);
+        int nCol = SZ(x[0]);
+
+        Matrix res(nCol, nRow);
+        REP(i,nCol) REP(j,nRow) {
+            res.x[i][j] = x[j][i];
+        }
+        return res;
+    }
+
+    // make matrix upper triangle
+    // copied from cp-algorithms
+    // https://cp-algorithms.com/linear_algebra/linear-system-gauss.html
+    void gauss() {
+        int n = SZ(x);
+        if (n == 0) return;
+
+        int m = SZ(x[0]);
+
+        vector<int> where(m, -1);
+        for (int col = 0, row = 0; col < m && row < n; ++col) {
+            int sel = row;
+            for (int i = row; i < n; ++i) {
+                if (abs(x[i][col]) > abs(x[sel][col]))
+                    sel = i;
+            }
+            if (abs(x[sel][col]) < 1e-9)   // change this for int?
+                continue;
+
+            for (int i = col; i < m; ++i)
+                swap(x[sel][i], x[row][i]);
+            where[col] = row;
+
+            for (int i = 0; i < n; ++i)
+                if (i != row) {
+                    T c = x[i][col] / x[row][col];  // change this for int?
+                    for (int j = col; j < m; ++j)
+                        x[i][j] -= x[row][j] * c;
+                }
+
+            // normalize this row to [0 ... 0 1 ....]
+            // needed for matrix inverse
+            T coef = x[row][col];
+            if (coef > 1e-9) {
+                for (int j = 0; j < m; ++j)
+                    x[row][j] /= coef;
+            }
+            ++row;
+        }
+    }
 };
-Matrix operator * (const Matrix &a, const Matrix &b) {
-    Matrix c(a.m, b.n);
-    REP(i,c.m) REP(j,c.n) {
-        REP(k,a.n)
-            c.x[i][j] = (c.x[i][j] + a.x[i][k] * b.x[k][j]) % MOD;
+
+template<typename T>
+Matrix<T> operator * (const Matrix<T> &a, const Matrix<T> &b) {
+    if (SZ(a.x) == 0 || SZ(b.x) == 0) return Matrix<T>(); // not sure if make sense?
+
+    Matrix<T> c(SZ(a.x), SZ(b[0]));
+    REP(i,SZ(c.x)) REP(j,SZ(c[0])) {
+        REP(k,SZ(a[0]))
+            c[i][j] += a[i][k] * b[k][j];
     }
     return c;
 }
 
-Matrix identity(int n) {
-    Matrix res(n, n);
+// https://en.wikipedia.org/wiki/Gaussian_elimination#Finding_the_inverse_of_a_matrix
+// Only works if matrix is invertible
+template<typename T>
+Matrix<T> inverse(Matrix<T> a) {
+    assert(SZ(a.x) == SZ(a[0]));
+    int n = SZ(a.x);
+
+    REP(i,n) {
+        vector<T> tmp(n);
+        tmp[i] = 1;
+        a[i].insert(a[i].end(), tmp.begin(), tmp.end());
+    }
+
+    a.gauss();
+
+    Matrix<T> inv(n, n);
+    REP(i,n) REP(j,n) {
+        inv[i][j] = a[i][j+n];
+    }
+    return inv;
+}
+
+template<typename T>
+Matrix<T> identity(int n) {
+    Matrix<T> res(n, n);
     REP(i,n) res.x[i][i] = 1;
     return res;
 }
