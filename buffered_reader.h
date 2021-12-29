e21c73023c5db7926b20397d2c56b5f6af1531e6
Trung Nguyen <ngthanhtrung23@gmail.com>
2021-12-29 13:48:41 +0800

    Rewrite buffered reader

--- buffered_reader.h
+++ buffered_reader.h
@@ -1,21 +1,33 @@
-int INP,AM,REACHEOF;
-#define BUFSIZE (1<<12)
-char BUF[BUFSIZE+1], *inp=BUF;
-#define GETCHAR(INP) { \
-    if(!*inp && !REACHEOF) { \
-        memset(BUF,0,sizeof BUF);\
-        int inpzzz = fread(BUF,1,BUFSIZE,stdin);\
-        if (inpzzz != BUFSIZE) REACHEOF = true;\
-        inp=BUF; \
-    } \
-    INP=*inp++; \
-}
-#define DIG(a) (((a)>='0')&&((a)<='9'))
-#define GN(j) { \
-    AM=0;\
-    GETCHAR(INP); while(!DIG(INP) && INP!='-') GETCHAR(INP);\
-    if (INP=='-') {AM=1;GETCHAR(INP);} \
-    j=INP-'0'; GETCHAR(INP); \
-    while(DIG(INP)){j=10*j+(INP-'0');GETCHAR(INP);} \
-    if (AM) j=-j;\
-}
+namespace IO {
+    const int BUFSIZE = 1<<14;
+    char buf[BUFSIZE + 1], *inp = buf;
+
+    bool reacheof;
+    char get_char() {
+        if (!*inp && !reacheof) {
+            memset(buf, 0, sizeof buf);
+            int tmp = fread(buf, 1, BUFSIZE, stdin);
+            if (tmp != BUFSIZE) reacheof = true;
+            inp = buf;
+        }
+        return *inp++;
+    }
+    template<typename T>
+    T get_int() {
+        int neg = 0;
+        int res = 0;
+        char c = get_char();
+        while (!std::isdigit(c) && c != '-' && c != '+') c = get_char();
+        if (c == '+') { neg = 0; }
+        else if (c == '-') { neg = 1; }
+        else res = c - '0';
+
+        c = get_char();
+        while (std::isdigit(c)) {
+            res = res * 10 + (c - '0');
+            c = get_char();
+        }
+        if (neg) res = -res;
+        return res;
+    }
+};
