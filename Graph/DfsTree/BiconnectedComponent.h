a326ec32694406061e57e192fbe9a0185caefa5f
Trung Nguyen <ngthanhtrung23@gmail.com>
2021-12-30 14:41:41 +0800

    Cleanup bridges & biconnected components

--- Graph/DfsTree/BiconnectedComponent.h
+++ Graph/DfsTree/BiconnectedComponent.h
@@ -1,13 +1,18 @@
 // Input graph: vector< vector<int> > a, int n
 // Note: 0-indexed
 // Usage: BiconnectedComponent bc; (bc.components is the list of components)
-
+//
+// This is biconnected components by edges (1 vertex can belong to
+// multiple components). For vertices biconnected component, remove
+// bridges and find components
+int n;
+vector<vector<int>> g;
 struct BiconnectedComponent {
-    vector<int> num, low, s;
+    vector<int> low, num, s;
     vector< vector<int> > components;
     int counter;
 
-    BiconnectedComponent() : num(n, -1), low(n, -1), counter(0) {
+    BiconnectedComponent() : low(n, -1), num(n, -1), counter(0) {
         for (int i = 0; i < n; i++)
             if (num[i] < 0)
                 dfs(i, 1);
@@ -15,14 +20,14 @@ struct BiconnectedComponent {
 
     void dfs(int x, int isRoot) {
         low[x] = num[x] = ++counter;
-        if (a[x].empty()) {
+        if (g[x].empty()) {
             components.push_back(vector<int>(1, x));
             return;
         }
         s.push_back(x);
 
-        for (int i = 0; i < (int) a[x].size(); i++) {
-            int y = a[x][i];
+        for (int i = 0; i < (int) g[x].size(); i++) {
+            int y = g[x][i];
             if (num[y] > -1) low[x] = min(low[x], num[y]);
             else {
                 dfs(y, 0);
--- Graph/DfsTree/BridgeArticulation.h
+++ Graph/DfsTree/BridgeArticulation.h
@@ -1,9 +1,13 @@
-// NOTE: DOES NOT WORK WHEN THERE ARE MULTIPLE EDGES! To fix, use map to count # occurrences of edges
 // Assume already have undirected graph vector< vector<int> > G with V vertices
 // Vertex index from 0
 // Usage:
 // UndirectedDfs tree;
 // Then you can use tree.bridges and tree.cuts
+//
+// Tested:
+// - https://judge.yosupo.jp/problem/two_edge_connected_components
+int n;
+vector<vector<int>> g;
 struct UndirectedDfs {
     vector<int> low, num, parent;
     vector<bool> articulation;
@@ -11,33 +15,42 @@ struct UndirectedDfs {
 
     vector< pair<int,int> > bridges;
     vector<int> cuts;
+    map<pair<int,int>, int> cnt_edges;
 
-    UndirectedDfs() : low(V, 0), num(V, -1), parent(V, 0), articulation(V, false),
+    UndirectedDfs() : low(n, 0), num(n, -1), parent(n, 0), articulation(n, false),
             counter(0), children(0) {
-        for(int i = 0; i < V; ++i) if (num[i] == -1) {
+        for (int u = 0; u < n; u++) {
+            for (int v : g[u]) {
+                cnt_edges[{u, v}] += 1;
+                cnt_edges[{v, u}] += 1;
+            }
+        }
+        for(int i = 0; i < n; ++i) if (num[i] == -1) {
             root = i; children = 0;
             dfs(i);
             articulation[root] = (children > 1);
         }
-        for(int i = 0; i < V; ++i)
+        for(int i = 0; i < n; ++i)
             if (articulation[i]) cuts.push_back(i);
     }
 private:
     void dfs(int u) {
         low[u] = num[u] = counter++;
-        for(int j = 0; j < (int) G[u].size(); ++j) {
-            int v = G[u][j];
+        for (int v : g[u]) {
             if (num[v] == -1) {
                 parent[v] = u;
                 if (u == root) children++;
                 dfs(v);
                 if (low[v] >= num[u])
                     articulation[u] = true;
-                if (low[v] > num[u]) bridges.push_back(make_pair(u, v));
+                if (low[v] > num[u]) {
+                    if (cnt_edges[{u, v}] == 2) {
+                        bridges.push_back(make_pair(u, v));
+                    }
+                }
                 low[u] = min(low[u], low[v]);
             } else if (v != parent[u])
                 low[u] = min(low[u], num[v]);
         }
     }
 };
-
