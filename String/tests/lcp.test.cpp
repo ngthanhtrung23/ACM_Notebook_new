9244eb31ff4de38d7329d4a01b518b86f112e99b
Trung Nguyen <ngthanhtrung23@gmail.com>
2022-01-06 05:55:24 +0800

    Fix overflow bug

--- String/tests/lcp.test.cpp
+++ String/tests/lcp.test.cpp
@@ -11,7 +11,7 @@ using namespace std;
 int32_t main() {
     ios::sync_with_stdio(0); cin.tie(0);
     string s; cin >> s;
-    int res = SZ(s) * (SZ(s) + 1) / 2;
+    long long res = SZ(s) * (long long)(SZ(s) + 1) / 2;
     SuffixArray sa(s);
     for (int x : sa.LCP) {
         res -= x;
