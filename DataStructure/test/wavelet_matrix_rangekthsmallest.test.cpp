d78787fc60a0e959f7d5faa6c05ffdd121ad9c48
Trung Nguyen <ngthanhtrung23@gmail.com>
2022-01-05 23:57:55 +0800

    Test Wavelet Matrix

---
+++ DataStructure/test/wavelet_matrix_rangekthsmallest.test.cpp
@@ -0,0 +1,21 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/range_kth_smallest"
+
+#include "bits/stdc++.h"
+using namespace std;
+
+#include "../WaveletMatrix.h"
+#include "../../buffered_reader.h"
+
+int32_t main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    int n, q; cin >> n >> q;
+    vector<int> a(n);
+    for (int& x : a) cin >> x;
+
+    WaveletMatrix wm(a);
+    while (q--) {
+        int l, r, k; cin >> l >> r >> k;
+        cout << wm.k_th(l, r, k) << '\n';
+    }
+    return 0;
+}
