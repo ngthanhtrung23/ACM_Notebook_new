0e780e5c467397c3078154cf404d00753b932f9d
Trung Nguyen <ngthanhtrung23@gmail.com>
2022-01-06 05:52:39 +0800

    Fix LCP test

--- String/tests/lcp.test.cpp
+++ String/tests/lcp.test.cpp
@@ -5,6 +5,9 @@ using namespace std;
 
 #include "../SuffixArray.h"
 
+#define REP(i, a) for (int i = 0, _##i = (a); i < _##i; ++i)
+#define SZ(x) ((int)(x).size())
+
 int32_t main() {
     ios::sync_with_stdio(0); cin.tie(0);
     string s; cin >> s;
