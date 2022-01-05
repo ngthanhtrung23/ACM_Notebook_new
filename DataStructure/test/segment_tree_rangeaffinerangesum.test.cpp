b8b559d250af3fa5ac9a8a6da87ba9fe7f4fc931
Trung Nguyen <ngthanhtrung23@gmail.com>
2022-01-05 23:49:51 +0800

    Lazy SegTree test

---
+++ DataStructure/test/segment_tree_rangeaffinerangesum.test.cpp
@@ -0,0 +1,76 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/range_affine_range_sum"
+
+#include "bits/stdc++.h"
+using namespace std;
+
+#include "../LazySegTree.h"
+#include "../../Math/modulo_anta.h"
+#include "../../buffered_reader.h"
+
+using modular = ModInt<998244353>;
+std::ostream& operator << (std::ostream& cout, const modular& m) {
+    cout << m.x;
+    return cout;
+}
+std::istream& operator >> (std::istream& cin, modular& m) {
+    cin >> m.x;
+    return cin;
+}
+
+struct Node {
+    modular sum, sz;
+};
+struct Lazy {
+    modular a, b;
+};
+
+Node op(Node l, Node r) {
+    return Node {
+        l.sum + r.sum,
+        l.sz + r.sz
+    };
+}
+Node e() {
+    return Node{0, 0};
+}
+
+Node apply(Lazy f, Node node) {
+    return Node{
+        f.a * node.sum + f.b * node.sz,
+        node.sz
+    };
+}
+Lazy combine(Lazy g, Lazy f) {
+    return Lazy {
+        f.a * g.a,
+        g.a * f.b + g.b
+    };
+}
+Lazy id() {
+    return Lazy{1, 0};
+}
+
+int32_t main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    int n, q; cin >> n >> q;
+    vector<Node> nodes(n);
+    for (auto& node : nodes) {
+        cin >> node.sum;
+        node.sz = 1;
+    }
+    LazySegTree<Node, op, e, Lazy, apply, combine, id> st(nodes);
+
+    while (q--) {
+        int typ; cin >> typ;
+        if (typ == 0) {
+            int l, r;
+            Lazy f;
+            cin >> l >> r >> f.a >> f.b;
+            st.apply(l, r, f);
+        } else {
+            int l, r; cin >> l >> r;
+            cout << st.prod(l, r).sum << '\n';
+        }
+    }
+    return 0;
+}
