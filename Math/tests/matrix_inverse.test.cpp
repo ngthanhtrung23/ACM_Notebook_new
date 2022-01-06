aca712353faa407c8a1e9fa152cd55cbc023b9c1
Trung Nguyen <ngthanhtrung23@gmail.com>
2022-01-06 13:54:21 +0800

    Matrix inverse test

---
+++ Math/tests/matrix_inverse.test.cpp
@@ -0,0 +1,30 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/inverse_matrix"
+
+#include <bits/stdc++.h>
+#include "../../atcoder/modint.hpp"
+using namespace std;
+using namespace atcoder;
+
+#include "../Matrix.h"
+#include "../../buffered_reader.h"
+
+#define REP(i, a) for (int i = 0, _##i = (a); i < _##i; ++i)
+
+int32_t main() {
+    ios::sync_with_stdio(0); cin.tie(0);
+    int n = IO::get_int<int>();
+    Matrix<modint998244353> a(n, n);
+    REP(i,n) REP(j,n) {
+        int x = IO::get_int<int>();
+        a[i][j] = x;
+    }
+    int rank = a.inverse();
+    if (rank < n) cout << -1 << '\n';
+    else {
+        REP(i,n) {
+            REP(j,n) cout << a[i][j].val() << ' ';
+            cout << '\n';
+        }
+    }
+    return 0;
+}
