029c64241261a90a326fb6497c79fc6ead344adc
Trung Nguyen <ngthanhtrung23@gmail.com>
2022-01-06 15:25:59 +0800

    BFS test

---
+++ Graph/tests/bfs.test.cpp
@@ -0,0 +1,28 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_11_C"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "../bfs.h"
+
+#define REP(i, a) for (int i = 0, _##i = (a); i < _##i; ++i)
+
+int main() {
+    int n; cin >> n;
+    Graph g(n);
+
+    REP(i,n) {
+        int u, k; cin >> u >> k;
+        --u;
+        while (k--) {
+            int v; cin >> v;
+            --v;
+            g.add_edge(u, v);
+        }
+    }
+    auto dist = g.bfs(0);
+    REP(i,n) {
+        cout << (i+1) << ' ' << dist[i] << '\n';
+    }
+    return 0;
+}
