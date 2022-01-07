c8dcc4107d637f3a3cc63ea0b2e03c17f6aafdc0
Trung Nguyen <ngthanhtrung23@gmail.com>
2022-01-07 17:45:15 +0800

    More assert in RMQ

--- DataStructure/RMQ.h
+++ DataStructure/RMQ.h
@@ -9,8 +9,8 @@
 // - https://judge.yosupo.jp/problem/staticrmq
 template<class T, T (*op) (T, T)> struct RMQ {
     RMQ() = default;
-    RMQ(const vector<T>& v) : t{v} {
-        for (int k = 1, n = (int) v.size(); (1<<k) <= n; ++k) {
+    RMQ(const vector<T>& v) : t{v}, n{(int) v.size()} {
+        for (int k = 1; (1<<k) <= n; ++k) {
             t.emplace_back(n - (1<<k) + 1);
             for (int i = 0; i + (1<<k) <= n; ++i) {
                 t[k][i] = op(t[k-1][i], t[k-1][i + (1<<(k-1))]);
@@ -21,13 +21,14 @@ template<class T, T (*op) (T, T)> struct RMQ {
     // get range [l, r-1]
     // doesn't work for empty range
     T get(int l, int r) const {
-        assert(l < r);
+        assert(0 <= l && l < r && r <= n);
         int k = __lg(r - l);
         return op(t[k][l], t[k][r - (1<<k)]);
     }
 
 private:
     vector<vector<T>> t;
+    int n;
 };
 template<class T> T _min(T a, T b) { return b < a ? b : a; }
 template<class T> T _max(T a, T b) { return a < b ? b : a; }
