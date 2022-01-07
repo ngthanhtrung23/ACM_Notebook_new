2828cd5d4ff999966691208f0a3c4fb9118ebff9
Trung Nguyen <ngthanhtrung23@gmail.com>
2022-01-07 16:03:49 +0800

    Simplify ford bellman

--- Graph/ford_bellman.h
+++ Graph/ford_bellman.h
@@ -1,6 +1,6 @@
-#include "bfs.h"
-
 // Ford Bellman, O(N*M)
+// Tested:
+// - https://cses.fi/problemset/task/1673
 using ll = long long;
 const ll INF = 4e18;
 struct Edge { int u, v; ll cost; }; // one-direction
@@ -10,7 +10,7 @@ std::pair<bool, ll> ford_bellman(int n, int start, int target, const vector<Edge
     int m = edges.size();
     std::vector<ll> f(n, INF);
     f[start] = 0;
-
+ 
     // Init f
     for (int turn = 0; turn < m; turn++) {
         for (auto [u, v, cost] : edges) {
@@ -19,7 +19,7 @@ std::pair<bool, ll> ford_bellman(int n, int start, int target, const vector<Edge
             }
         }
     }
-
+ 
     // Find all unbounded vertices
     auto cur_f = f;
     for (int turn = 0; turn < m; turn++) {
@@ -29,21 +29,21 @@ std::pair<bool, ll> ford_bellman(int n, int start, int target, const vector<Edge
             }
         }
     }
-
-    vector<int> unbounded;
+ 
+    // Set all unbounded vertices to -INF
     for (int i = 0; i < n; i++) {
         if (f[i] != cur_f[i]) {
-            unbounded.push_back(i);
+            f[i] = -INF;
         }
     }
-
-    // Check if target is reachable from unbounded vertices
-    Graph g(n);
-    for (auto [u, v, cost] : edges) {
-        g.add_edge(u, v);
+ 
+    // Re-update all f
+    for (int turn = 0; turn < m; turn++) {
+        for (auto [u, v, cost] : edges) {
+            if (f[u] != INF && f[v] > f[u] + cost) {
+                f[v] = f[u] + cost;
+            }
+        }
     }
-    auto dists = g.bfs(unbounded);
-
-    return {dists[target] == -1, f[target]};
+    return {f[target] == cur_f[target], f[target]};
 }
-
