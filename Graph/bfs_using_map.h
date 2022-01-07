27b33913902cceaeca3ebea4df3f1f167d66e4bd
Trung Nguyen <ngthanhtrung23@gmail.com>
2022-01-07 13:20:04 +0800

    BFS should also return path

--- Graph/bfs_using_map.h
+++ Graph/bfs_using_map.h
@@ -1,18 +1,26 @@
 // BFS using map, so extra log factor
 //
+// Note:
+// - Can use unordered_map for small speed improvement
+//   (see unordered_map_pair.h for how to implement hash for other types)
+//
 // Tested:
 // - https://oj.vnoi.info/problem/vmunch
+// - https://cses.fi/problemset/task/1193/
 template<typename T>
 struct Graph {
     void add_edge(const T& u, const T& v) {
         g[u].push_back(v);
     }
 
-    // return shortest distance from start -> target
-    // If no path -> returns -1
-    int bfs(const T& start, const T& target) {
+    // return
+    // - shortest distance from start -> target
+    // - path
+    // If no path -> returns {-1, {}}
+    std::pair<int, std::vector<T>> bfs(const T& start, const T& target) {
         queue<T> qu;
-        map<T, int> dist;
+        std::map<T, int> dist;
+        std::map<T, T> trace;
 
         dist[start] = 0;
         qu.push(start);
@@ -20,18 +28,25 @@ struct Graph {
         while (!qu.empty()) {
             auto u = qu.front(); qu.pop();
             if (u == target) {
-                return dist[u];
+                std::vector<T> path;
+                for (auto cur = target; cur != start; cur = trace[cur]) {
+                    path.push_back(cur);
+                }
+                path.push_back(start);
+                reverse(path.begin(), path.end());
+                return {dist[u], path};
             }
 
             for (const auto& v : g[u]) {
                 if (!dist.count(v)) {
                     dist[v] = dist[u] + 1;
+                    trace[v] = u;
                     qu.push(v);
                 }
             }
         }
 
-        return -1;
+        return {-1, {}};
     }
 
 // private:
