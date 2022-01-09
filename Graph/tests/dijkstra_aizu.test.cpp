d0f709150b480afe6051cb14e424934bfdacae10
Trung Nguyen <ngthanhtrung23@gmail.com>
2022-01-09 23:55:11 +0800

    Shortest path tests

---
+++ Graph/tests/dijkstra_aizu.test.cpp
@@ -0,0 +1,21 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_1_A"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "../dijkstra.h"
+
+int main() {
+    int n, m, start; cin >> n >> m >> start;
+    vector< vector<pair<int, ll>> > g(n);
+    while (m--) {
+        int u, v, c; cin >> u >> v >> c;
+        g[u].push_back({v, c});
+    }
+    auto [dist, trace] = dijkstra(g, start);
+    for (auto d : dist) {
+        if (d == INF) cout << "INF\n";
+        else cout << d << '\n';
+    }
+    return 0;
+}
---
+++ Graph/tests/floyd_aizu.test.cpp
@@ -0,0 +1,34 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_1_C"
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "../floyd.h"
+
+int main() {
+    int n, m; cin >> n >> m;
+    vector<vector<ll>> c(n, vector<ll> (n, INF));
+    for (int i = 0; i < n; i++) c[i][i] = 0;
+
+    while (m--) {
+        int u, v; ll cost; cin >> u >> v >> cost;
+        c[u][v] = min(c[u][v], cost);
+    }
+
+    Floyd f(n, c);
+    for (int i = 0; i < n; i++) {
+        if (f.c[i][i] < 0) {
+            cout << "NEGATIVE CYCLE" << endl;
+            return 0;
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (f.c[i][j] == INF) cout << "INF";
+            else cout << f.c[i][j];
+            cout << ' ';
+        }
+        cout << endl;
+    }
+    return 0;
+}
