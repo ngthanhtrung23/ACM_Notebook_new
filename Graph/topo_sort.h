7741922ed58494b95130b2a326e83436c8975c57
Trung Nguyen <ngthanhtrung23@gmail.com>
2022-01-09 11:44:38 +0800

    Update topo sort

--- Graph/topo_sort.h
+++ Graph/topo_sort.h
@@ -1,8 +1,12 @@
 // Topo sort
 // returns: <has topo sort?, topo order>
 //
+// Notes:
+// - To find lexicographically min -> change queue<int> qu to set
+//
 // Tested:
 // - https://cses.fi/problemset/task/1679/
+// - https://cses.fi/problemset/task/1757/
 std::pair<bool, std::vector<int>> topo_sort(const std::vector<std::vector<int>>& g) {
     int n = g.size();
     // init in_deg
@@ -18,17 +22,16 @@ std::pair<bool, std::vector<int>> topo_sort(const std::vector<std::vector<int>>&
     std::queue<int> qu;
     for (int u = 0; u < n; u++) {
         if (in_deg[u] == 0) {
-            res.push_back(u);
             qu.push(u);
         }
     }
 
     while (!qu.empty()) {
         int u = qu.front(); qu.pop();
+        res.push_back(u);
         for (int v : g[u]) {
             in_deg[v]--;
             if (in_deg[v] == 0) {
-                res.push_back(v);
                 qu.push(v);
             }
         }
