f41a361c72b5a3128c8b45491328993eccb00d3a
Trung Nguyen <ngthanhtrung23@gmail.com>
2022-06-19 02:44:37 +0800

    Update SOS

--- Misc/magic.cpp
+++ Misc/magic.cpp
@@ -1,14 +1,19 @@
+#include <vector>
+
 // Given array D[i]
 // Calculate D'[i] = sum( D[j] | j is subset of i )
-//
-// If we want to calculate D'[i] = sum( D[j] | j is superset of i ),
-// change D[t|i] += D[f|i] --> D[f|i] += D[t|i]
+void sum_of_subset(std::vector<int>& D, int K) {
+    for (int i = 0; i < K; ++i)
+        for (int mask = 0; mask < (1 << K); ++mask)
+            if ((mask >> i) & 1) D[mask] += D[mask ^ (1 << i)];
+}
 
-void magic() {
-    for(b=0; b<K; b++) {
-        int f=0, t=(1<<b);
-        for(g=0; g<(1<<(K-1-b)); f+=(1<<(b+1)), t+=(1<<(b+1)), g++)
-            for(i=0; i<(1<<b); i++)
-                D[t|i] += D[f|i];
-    }
+// Given array D[i]
+// Calculate D'[i] = sum( D[j] | j is subset of i )
+void sum_of_superset(std::vector<int>& D, int K) {
+    std::reverse(D.begin(), D.end());
+    for (int i = 0; i < K; ++i)
+        for (int mask = 0; mask < (1 << K); ++mask)
+            if ((mask >> i) & 1) D[mask] += D[mask ^ (1 << i)];
+    std::reverse(D.begin(), D.end());
 }
