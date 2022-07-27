cf4e6ad82c1cc2dc3e93728cdf2e869f565cf215
Trung Nguyen <ngthanhtrung23@gmail.com>
2022-07-27 18:36:50 +0800

    Fix typo

--- Misc/magic.cpp
+++ Misc/magic.cpp
@@ -9,7 +9,7 @@ void sum_of_subset(std::vector<int>& D, int K) {
 }
 
 // Given array D[i]
-// Calculate D'[i] = sum( D[j] | j is subset of i )
+// Calculate D'[i] = sum( D[j] | j is superset of i )
 void sum_of_superset(std::vector<int>& D, int K) {
     std::reverse(D.begin(), D.end());
     for (int i = 0; i < K; ++i)
