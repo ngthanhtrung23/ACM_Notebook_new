d493d6844e80aec653581fc5fc49013b65908910
Trung Nguyen <ngthanhtrung23@gmail.com>
2022-11-13 20:55:13 +0800

    Rabin miller: fix bug

--- Math/Prime/RabinMiller.h
+++ Math/Prime/RabinMiller.h
@@ -9,18 +9,20 @@
 // 2. Sieve and check for small primes separately
 // 3. Check divisible by first 50 primes (see https://oj.vnoi.info/src/1816262)
 
+#include <initializer_list>
 
-unsigned long long mult(unsigned long long x, unsigned long long y, unsigned long long mod) {
+using uint = unsigned long long;
+uint mult(uint x, uint y, uint mod) {
     return __int128_t(x) * y % mod;
 }
 
-unsigned long long powMod(unsigned long long x, unsigned long long p, unsigned long long mod) {
+uint powMod(uint x, uint p, uint mod) {
     if (p == 0) return 1;
     if (p % 2) return mult(x, powMod(x, p - 1, mod), mod);
     return powMod(mult(x, x, mod), p / 2, mod);
 }
 
-bool checkMillerRabin(unsigned long long x, unsigned long long mod, unsigned long long s, int k) {
+bool checkMillerRabin(uint x, uint mod, uint s, int k) {
     x = powMod(x, s, mod);
     if (x == 1) return true;
     while(k--) {
@@ -30,23 +32,24 @@ bool checkMillerRabin(unsigned long long x, unsigned long long mod, unsigned lon
     }
     return false;
 }
-bool is_prime(unsigned long long x) {
+
+bool is_prime(uint x) {
     if (x == 2 || x == 3 || x == 5 || x == 7) return true;
     if (x % 2 == 0 || x % 3 == 0 || x % 5 == 0 || x % 7 == 0) return false;
     if (x < 121) return x > 1;
 
-    unsigned long long s = x - 1;
+    uint s = x - 1;
     int k = 0;
     while (s % 2 == 0) {
         s /= 2;
         k++;
     }
     if (x < 1LL << 32) {
-        for (unsigned long long z : {2, 7, 61}) {
+        for (uint z : {2, 7, 61}) {
             if (!checkMillerRabin(z, x, s, k)) return false;
         }
     } else {
-        for (unsigned long long z : {2, 325, 9375, 28178}) {
+        for (uint z : {2, 325, 9375, 28178, 450775, 9780504, 1795265022}) {
             if (!checkMillerRabin(z, x, s, k)) return false;
         }
     }
