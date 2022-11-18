dd909b063e09331624962427a897028c96410bf2
Trung Nguyen <ngthanhtrung23@gmail.com>
2022-11-18 13:40:31 +0800

    Add rabin miller for 32 bit numbers

---
+++ Math/Prime/RabinMiller32.h
@@ -0,0 +1,42 @@
+inline unsigned mod_mult(unsigned a, unsigned b, unsigned m) {
+  return (uint64_t)a*b%m;
+}
+ 
+unsigned mod_pow(unsigned a, uint64_t b, unsigned m) {
+  unsigned ret = 1;
+  for(;;) {
+    if (b&1) ret = mod_mult(ret, a, m);
+    if (!(b>>=1)) return ret;
+    a = mod_mult(a, a, m);
+  }
+}
+ 
+bool is_prime(unsigned n) {
+  if (n <= 3) return (n >= 2);
+  static const unsigned small[] = {
+        2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67,
+        71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139,
+  };
+  for (int i = 0; i < sizeof(small)/sizeof(unsigned); i++) {
+    if (n%small[i] == 0) return n == small[i];
+  }
+ 
+  // Jaeschke93 showed that 2,7,61 suffice for n < 4,759,123,141.
+  static const unsigned millerrabin1[] = {2, 7, 61};
+  static const unsigned millerrabin2[] = {11000544, 31481107};
+  unsigned s = n-1, r = 0;
+  while (s%2 == 0) {s /= 2; r++;}
+ 
+  const auto& test = (n < 316349281 ? millerrabin2 : millerrabin1);
+  for (int i = 0, j; i < sizeof(test)/sizeof(unsigned); i++) {
+    unsigned md = mod_pow(test[i], s, n);
+    if (md == 1) continue;
+    for (j = 1; j < r; j++) {
+      if (md == n-1) break;
+      md = mod_mult(md, md, n);
+    }
+    if (md != n-1) return false;
+  }
+ 
+  return true;
+}
