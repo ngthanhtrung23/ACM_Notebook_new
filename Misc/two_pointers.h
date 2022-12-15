1279eb898e9bacc2fa92816c8639bd33dcf8ead0
Trung Nguyen <ngthanhtrung23@gmail.com>
2022-12-15 16:41:42 +0800

    Two pointers

---
+++ Misc/two_pointers.h
@@ -0,0 +1,31 @@
+// O(N) 2 pointers
+// Find optimal substring that satisfy a predicate while minimizing a cost function
+//
+// - pred() = whether current segment satisfy condition
+// - cost(l, r) = cost of [l, r]
+// - add(i) = add i to current segment
+// - rem(i) = remove i from current segment
+// - indices from 0 to n-1
+//
+// Returns: <best cost, left, right> where [left, right] is substring with
+//   mincost or [-1, -1] if no solution
+//
+// Two pointers {{{
+template<typename Pred, typename Cost, typename Add, typename Rem>
+tuple<long long, int, int> two_pointers(int n, Pred pred, Cost cost, Add add, Rem rem) {
+    tuple<long long, int, int> res {LLONG_MAX, -1, -1};
+
+    // current window is [l, r]
+    for (int l = 0, r = -1; l < n; ++l) {
+        while (r+1 < n && !pred()) {
+            ++r;
+            add(r);
+        }
+        if (pred()) {
+            res = min(res, {cost(l, r), l, r});
+        }
+        rem(l);
+    }
+    return res;
+}
+// }}}
