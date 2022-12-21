23b1b970ab5266d1380ad0ba766f3fb7ecbd5373
Trung Nguyen <ngthanhtrung23@gmail.com>
2022-12-21 09:44:46 +0800

    Tip to make order_statistic tree allow equal elements

--- DataStructure/STL/order_statistic.cpp
+++ DataStructure/STL/order_statistic.cpp
@@ -3,6 +3,7 @@
 
 using namespace __gnu_pbds;
 
+// for multiset, change less -> less_equal
 typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
 
 int main() { 
