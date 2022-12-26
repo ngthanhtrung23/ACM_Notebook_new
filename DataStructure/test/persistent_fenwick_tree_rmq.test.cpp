08afb753454d57f286ae10e1b89b72401d832563
Trung Nguyen <ngthanhtrung23@gmail.com>
2022-12-26 17:35:16 +0800

    Persistent Fenwick Tree test

---
+++ DataStructure/test/persistent_fenwick_tree_rmq.test.cpp
@@ -0,0 +1,30 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/staticrmq"
+#include "../../template.h"
+#include "../PartiallyPersistentFenwick.h"
+
+struct Data {
+    int x;
+    Data() : x(INT_MAX) {}
+    Data(int _x) : x(_x) {}
+};
+Data operator + (const Data& a, const Data& b) {
+    return Data{min(a.x, b.x)};
+}
+bool operator < (const Data&, const Data&) {
+    return false;
+}
+
+void solve() {
+    int n, q; cin >> n >> q;
+    vector<int> a(n);
+    REP(i,n) cin >> a[i];
+
+    PartiallyPersistentFenwick<Data> fen(n);
+    FORD(i,n-1,0) fen.update(n-i, i, Data{a[i]});
+
+    while (q--) {
+        int l, r; cin >> l >> r;
+        auto res = fen.get(n-l, r);
+        cout << res.x << '\n';
+    }
+}
