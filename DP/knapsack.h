65a17201b415442e1544526d6e2f171387a4c9d9
Trung Nguyen <ngthanhtrung23@gmail.com>
2023-02-03 11:05:26 +0800

    Unbounded knapsack DP

--- DP/knapsack.h
+++ DP/knapsack.h
@@ -16,3 +16,22 @@ int knapsack01(
     return *max_element(f.begin(), f.end());
 }
 // }}}
+// Knapsack unbounded {{{
+// Select subset of items, such that sum(weights) <= capacity
+// and sum(values) is maximum
+// An item can be selected unlimited number of times
+int knapsack_unbounded(
+        int capacity,
+        const std::vector<int>& weights,
+        const std::vector<int>& values) {
+    int n = weights.size();
+    std::vector<int> f(capacity + 1, 0);
+    for (int i = 0; i < n; ++i) {
+        for (int j = weights[i]; j <= capacity; ++j) {
+            f[j] = max(f[j], f[j-weights[i]] + values[i]);
+        }
+    }
+
+    return *max_element(f.begin(), f.end());
+}
+// }}}
