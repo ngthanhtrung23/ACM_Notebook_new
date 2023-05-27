98fe9b5a7e2dc0f289f4d3bc024d6d524f37fdde
Trung Nguyen <ngthanhtrung23@gmail.com>
2023-05-27 17:03:38 +0800

    Reverse int -> int

--- Misc/number_utils.h
+++ Misc/number_utils.h
@@ -9,3 +9,13 @@ vector<int> get_digits(int n) {
     return res;
 }
 // }}}
+// reverse an integer {{{
+int reverse(int n) {
+    int res = 0;
+    while (n > 0) {
+        res = res * 10 + n % 10;
+        n /= 10;
+    }
+    return res;
+}
+// }}}
